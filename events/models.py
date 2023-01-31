a9a4e534503dd328d8bfb66ab13ff203531ababa
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2023-01-31 15:59:03 +0100

    Validators

--- events/models.py
+++ events/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 # Create your models here.
+from django.core.exceptions import ValidationError
+from users.models import *
+from datetime import datetime
+
+def titleValidator(value):
+    
+    if not value.istitle():
+        raise ValidationError(
+            "Title must be contain capital letters"
+        )
+
+def dateValidator(value):
+    if not value >= datetime.now().date():
+        raise ValidationError("date must be in the future")
 
 class Event(models.Model):
     CATEGORY_CHOICES = (
@@ -8,15 +22,18 @@ class Event(models.Model):
         ('Sport', 'Sport'),
     )
 
-    title = models.CharField(max_length=255, null=True)
+    title = models.CharField(max_length=255, null=True , validators=[titleValidator])
     description = models.TextField()
     eventImage = models.ImageField(upload_to='images/', blank=True)
 
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=8)
     state = models.BooleanField(default=False)
     nbrParticipants = models.IntegerField(default=0)
-    eventDate = models.DateField()
+    eventDate = models.DateField(validators=[dateValidator])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+    organizer = models.ForeignKey(Person,
+    on_delete=models.CASCADE)
 
--- users/models.py
+++ users/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxLengthValidator,MinLengthValidator
+from django.core.validators import RegexValidator
+from  django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+def is_mail_esprit(value):
+    if str(value).endswith('@esprit.tn') == False:
+        raise ValidationError(
+            "Your email must be @esprit.tn"
+        )
 
 
 # Create your models here.
@@ -7,11 +17,16 @@ class Person(AbstractUser):
     cin = models.CharField(
         "CIN",
         primary_key=True,
-        max_length=8
+        max_length=8,
+        validators=[
+            MaxLengthValidator(8, message="Verify length"),
+            MinLengthValidator(8, message="Verify length"),
+            RegexValidator(r'^[0-9]$' , message="Just number")
+        ]
     )
     username = models.CharField("Username", max_length=255, unique=True)
     email = models.EmailField(
-        unique=True
+        unique=True, validators=[is_mail_esprit]
     )
 
     USERNAME_FIELD = 'username'
